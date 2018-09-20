a45c69a3601135f80ddeb05acbc51a1b7508b2bf
Astrocat2374 <sugiyama.evan@gmail.com>
2018-09-19 15:13:32 -1000

    I'm dead

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-var song = document.createElement('div')
+console.log('data', data);
+
+//Renders songs
+
+var newSong;
 var songBlock = document.querySelector('#data');
 
-var songData = data.map((i)=>{
-    songBlock.appendChild(song);
-    songBlock.innerHTML = songData;
-    return songTemplate = `
-    <img src=${image}></img>
-    <h2>${title}</h2>
-    <h3>${artist}</h3>
+for (i = 0; i < data.length; i++) {
+    songTemplate = `
+    <img src=${data[i].image} class='image'></img>
+    <h2>${data[i].title}</h2>
+    <h3>${data[i].artist}</h3>
     ` ;
-})
+    newSong = document.createElement('div');
+    songBlock.appendChild(newSong).id = i;
+    document.getElementById(i).className = 'song';
+    newSong.innerHTML = songTemplate;
+}
+
+//Filter script
+
--- data.js
+++ data.js
@@ -1,7 +1,31 @@
 const data = [
     {
-        title: 'Somewhere Beyond the Sea',
+        title: "Somewhere Beyond the Sea",
         artist: 'Bobby Darin',
         genre: 'Jazz',
         image: 'images/somewhere_beyond.jpg'
+    },
+    {
+        title: "That's Life",
+        artist: 'Frank Sinatra',
+        genre: 'Classic pop',
+        image: 'images/thats_life.jpg'
+    },
+    {
+        title: "Crawl Out Through the Fallout",
+        artist: 'Sheldon Allman',
+        genre: 'Classic pop',
+        image: 'images/crawl_out.jpg'
+    },
+    {
+        title: "Crazy He Calls Me",
+        artist: 'Billie Holiday',
+        genre: 'Jazz',
+        image: 'images/crazy.jpg'
+    },
+    {
+        title: "Dear Hearts and Gentle People",
+        artist: 'Bob Crosby',
+        genre: 'Jazz',
+        image: 'images/dear_hearts.jpg'
     }];
