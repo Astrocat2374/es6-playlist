45146a87a374943a0ceaa0f448bd5753712b05b3
Astrocat2374 <sugiyama.evan@gmail.com>
2018-09-24 11:21:39 -1000

    Blyat

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ console.log('data', data);
 var newSong;
 var songBlock = document.querySelector('#data');
 
-for (i = 0; i < data.length; i++) {
+/*for (i = 0; i < data.length; i++) {
     songTemplate = `
     <img src=${data[i].image} class='image'></img>
     <h2>${data[i].title}</h2>
@@ -16,6 +16,20 @@ for (i = 0; i < data.length; i++) {
     document.getElementById(i).className = 'song';
     newSong.innerHTML = songTemplate;
 }
+*/
 
 //Filter script
-
+var filterJazz = data.filter(song => {
+    return song.genre === 'Jazz';
+}).map(function(jazzMusic){
+    console.log('jazzMusic', jazzMusic);
+    songTemplate = `
+    <img src=${jazzMusic.image} class='image'></img>
+    <h2>${jazzMusic.title}</h2>
+    <h3>${jazzMusic.artist}</h3>
+    ` ;
+    newSong = document.createElement('div');
+    songBlock.appendChild(newSong).id = jazzMusic;
+    document.getElementById(jazzMusic).className = 'song';
+    newSong.innerHTML = songTemplate;
+})
