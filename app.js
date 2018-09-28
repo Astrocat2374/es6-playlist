125461fc1c378a4765479c3e2ea4909978d4e1b4
Astrocat2374 <sugiyama.evan@gmail.com>
2018-09-27 19:51:18 -1000

    MVP

--- app.js
+++ app.js
@@ -3,33 +3,84 @@ console.log('data', data);
 //Renders songs
 
 var newSong;
-var songBlock = document.querySelector('#data');
 
-/*for (i = 0; i < data.length; i++) {
-    songTemplate = `
-    <img src=${data[i].image} class='image'></img>
-    <h2>${data[i].title}</h2>
-    <h3>${data[i].artist}</h3>
-    ` ;
-    newSong = document.createElement('div');
-    songBlock.appendChild(newSong).id = i;
-    document.getElementById(i).className = 'song';
-    newSong.innerHTML = songTemplate;
+
+function render () {
+    console.log('Rendered Music')
+    data.map(allMusic => {
+        let songBlock = document.querySelector('#data');
+        songTemplate = `
+        <img src=${allMusic.image} class='image'></img>
+        <h2>${allMusic.title}</h2>
+        <h3>${allMusic.artist}</h3>
+        <br>
+        <br>
+        <br>
+        <audio controls src=${allMusic.music}>Your web browser does not support the <code>Audio</code> element</audio>
+        ` ;
+        newSong = document.createElement('div');
+        songBlock.appendChild(newSong).className = 'song';
+        newSong.innerHTML = songTemplate;
+    })
 }
-*/
 
-//Filter script
-var filterJazz = data.filter(song => {
-    return song.genre === 'Jazz';
-}).map(function(jazzMusic){
-    console.log('jazzMusic', jazzMusic);
-    songTemplate = `
-    <img src=${jazzMusic.image} class='image'></img>
-    <h2>${jazzMusic.title}</h2>
-    <h3>${jazzMusic.artist}</h3>
-    ` ;
-    newSong = document.createElement('div');
-    songBlock.appendChild(newSong).id = jazzMusic;
-    document.getElementById(jazzMusic).className = 'song';
-    newSong.innerHTML = songTemplate;
+window.onload = render;
+
+//Filter All
+
+const filterAll = document.querySelector('#filter-all').addEventListener('click', function() {
+    let child = document.querySelector('#data');
+    let parent = document.querySelector('#data-parent');
+    //Removes previously rendered music
+    parent.removeChild(child);
+    console.log('Deleted');
+    child = document.createElement('div');
+    parent.appendChild(child).id = 'data';
+    console.log('Added new div');
+    data.map(allMusic => {
+        let songBlock = document.querySelector('#data');
+        console.log('allMusic', allMusic);
+        songTemplate = `
+        <img src=${allMusic.image} class='image'></img>
+        <h2>${allMusic.title}</h2>
+        <h3>${allMusic.artist}</h3>
+        <br>
+        <br>
+        <br>
+        <audio controls src=${allMusic.music}>Your web browser does not support the <code>Audio</code> element</audio>
+        ` ;
+        newSong = document.createElement('div');
+        songBlock.appendChild(newSong).className = 'song';
+        newSong.innerHTML = songTemplate;
+    })
+})
+
+//Filter rock script
+
+const filterRock = document.querySelector('#filter-rock').addEventListener('click', function() {
+    let child = document.querySelector('#data');
+    let parent = document.querySelector('#data-parent');
+    parent.removeChild(child);
+    console.log('Deleted');
+    child = document.createElement('div');
+    parent.appendChild(child).id = 'data';
+    console.log('Added new div');
+    data.filter(song => {
+        return song.genre === 'Rock';
+    }).map(function(rockMusic){
+        let songBlock = document.querySelector('#data');
+        console.log('rockMusic', rockMusic);
+        songTemplate = `
+        <img src=${rockMusic.image} class='image'></img>
+        <h2>${rockMusic.title}</h2>
+        <h3>${rockMusic.artist}</h3>
+        <br>
+        <br>
+        <br>
+        <audio controls src=${rockMusic.music}>Your web browser does not support the <code>Audio</code> element</audio>
+        ` ;
+        newSong = document.createElement('div');
+        songBlock.appendChild(newSong).className = 'song';
+        newSong.innerHTML = songTemplate;
+    })
 })
--- data.js
+++ data.js
@@ -1,31 +1,142 @@
 const data = [
     {
-        title: "Somewhere Beyond the Sea",
-        artist: 'Bobby Darin',
-        genre: 'Jazz',
-        image: 'images/somewhere_beyond.jpg'
+        title: "Anyway You Want It",
+        artist: 'Journey',
+        genre: 'Rock',
+        image: 'images/anyway.jpg',
+        music: 'music/AWYWI.mp3'
     },
     {
-        title: "That's Life",
-        artist: 'Frank Sinatra',
-        genre: 'Classic pop',
-        image: 'images/thats_life.jpg'
+        title: "Eye in the Sky",
+        artist: 'Alan Parsons',
+        genre: 'Rock',
+        image: 'images/eye_in_sky.jpg',
+        music: 'music/EITS.mp3'
     },
     {
-        title: "Crawl Out Through the Fallout",
-        artist: 'Sheldon Allman',
-        genre: 'Classic pop',
-        image: 'images/crawl_out.jpg'
+        title: "Bohemian Rhapsody",
+        artist: 'Queen',
+        genre: 'Rock',
+        image: 'images/bohemian.jpg',
+        music: 'music/BR.mp3'
     },
     {
-        title: "Crazy He Calls Me",
-        artist: 'Billie Holiday',
-        genre: 'Jazz',
-        image: 'images/crazy.jpg'
+        title: "Seven Nation Army",
+        artist: 'The White Stripes',
+        genre: 'Rock',
+        image: 'images/seven_nation.jpg',
+        music: 'music/SNA.mp3'
     },
     {
-        title: "Dear Hearts and Gentle People",
-        artist: 'Bob Crosby',
-        genre: 'Jazz',
-        image: 'images/dear_hearts.jpg'
-    }];
+        title: "Here I Go Again",
+        artist: 'Whitesnake',
+        genre: 'Hard Rock',
+        image: 'images/here_I_go.jpg',
+        music: 'music/HIGA.mp3'
+    },
+    {
+        title: "Fortunate Son",
+        artist: 'Creedence Clearwater Revival',
+        genre: 'Rock',
+        image: 'images/vietnam.jpg',
+        music: 'music/FS.mp3'
+    },
+    {
+        title: "Stairway to Heaven",
+        artist: 'Led Zeppelin',
+        genre: 'Hard Rock',
+        image: 'images/stairway.jpg',
+        music: 'music/STH.mp3'
+    },
+    {
+        title: "Sympathy for the Devil",
+        artist: 'The Rolling Stones',
+        genre: 'Classic Rock',
+        image: 'images/sympathy.jpg',
+        music: 'music/SFTD.mp3'
+    },
+    {
+        title: "House of the Rising Sun",
+        artist: 'The Animals',
+        genre: 'Rock',
+        image: 'images/rising_sun.jpg',
+        music: 'music/HOTRS.mp3'
+    },
+    {
+        title: "Livin on a Prayer",
+        artist: 'Bon Jovi',
+        genre: 'Rock',
+        image: 'images/livin_on_a_prayer.jpg',
+        music: 'music/LOAP.mp3'
+    },
+    {
+        title: "In the Navy",
+        artist: 'Village People',
+        genre: 'Dance',
+        image: 'images/in_the_navy.jpg',
+        music: 'music/ITN.mp3'
+    },
+    {
+        title: "Hotel California",
+        artist: 'Eagles',
+        genre: 'Classic Rock',
+        image: 'images/hotel_california.jpg',
+        music: 'music/HC.mp3'
+    },
+    {
+        title: "Hooked on a Feeling",
+        artist: 'Blue Swede',
+        genre: 'Pop Rock',
+        image: 'images/hooked.jpg',
+        music: 'music/HOAF.mp3'
+    },
+    {
+        title: "Stuck in the Middle with You",
+        artist: 'Stealers Wheel',
+        genre: 'Pop Rock',
+        image: 'images/stuck_in_the_middle.jpg',
+        music: 'music/SITMWY.mp3'
+    },
+    {
+        title: "Sweet Child o' Mine",
+        artist: 'Guns and Roses',
+        genre: 'Rock',
+        image: 'images/sweet_child.jpg',
+        music: 'music/SCOM.mp3'
+    },
+    {
+        title: "Don't Stop Me Now",
+        artist: 'Queen',
+        genre: 'Classic Rock',
+        image: 'images/dont_stop_me.jpg',
+        music: 'music/DSMN.mp3'
+    },
+    {
+        title: "Paradise City",
+        artist: 'Guns and Roses',
+        genre: 'Hard Rock',
+        image: 'images/sweet_child.jpg',
+        music: 'music/PC.mp3'
+    },
+    {
+        title: "Dream On",
+        artist: 'Aerosmith',
+        genre: 'Hard Rock',
+        image: 'images/dream.jpg',
+        music: 'music/DO.mp3'
+    },
+    {
+        title: "More Than a Feeling",
+        artist: 'Boston',
+        genre: 'Rock',
+        image: 'images/marianne.jpg',
+        music: 'music/MTAF.mp3'
+    },
+    {
+        title: "Highway To Hell",
+        artist: 'ACDC',
+        genre: 'Hard Rock',
+        image: 'images/highway.jpg',
+        music: 'music/HTH.mp3'
+    }
+];
